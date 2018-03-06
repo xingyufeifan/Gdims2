5a6f6ecc51fc6123c86cbc52cfce8894093c74d2
baohongyan <657175417@qq.com>
2018-03-06 18:00:04 +0800

    片区专管1

---
+++ Gdims2/NDAreaOfficerModule/NDAreaOfficerViewController.h
@@ -0,0 +1,18 @@
+//
+//  NDAreaOfficerViewController.h
+//  Gdims2
+//
+//  Created by 包宏燕 on 2018/3/6.
+//  Copyright © 2018年 name. All rights reserved.
+//
+
+#import "NDViewController.h"
+
+/**
+ * 片区专管员
+ */
+@interface NDAreaOfficerViewController : NDViewController
+
+@property (nonatomic, strong) CLLocation * location;
+
+@end
---
+++ Gdims2/NDAreaOfficerModule/NDAreaOfficerViewController.m
@@ -0,0 +1,37 @@
+//
+//  NDAreaOfficerViewController.m
+//  Gdims2
+//
+//  Created by 包宏燕 on 2018/3/6.
+//  Copyright © 2018年 name. All rights reserved.
+//
+
+#import "NDAreaOfficerViewController.h"
+
+@interface NDAreaOfficerViewController ()
+
+@end
+
+@implementation NDAreaOfficerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
